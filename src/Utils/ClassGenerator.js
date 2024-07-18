15815603985d3ddf4d0b70202c45fc3c698782b0
Santi López <s.lopez_@outlook.com>
2024-07-18 19:47:27 +0200

    icons

--- src/Utils/ClassGenerator.js
+++ src/Utils/ClassGenerator.js
@@ -1,7 +1,12 @@
 export function colGrid(clasesCol) {
-    return clasesCol + " px-3.5 ";
+    return clasesCol + " px-8";
 }
 
 export function containerGrid(clasesContainer = '') {
     return "element max-w-screen-xl mx-auto flex flex-wrap relative " + clasesContainer;
+}
+
+export function rowGrid(clasesRow = '') {
+    return " -mx-8 flex " + clasesRow;
+
 }
